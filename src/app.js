33aad5d543686e3958ae3827dba85f4a7fd06823
JigarVaghela784 <jigarvag007@gmail.com>
2023-02-17 17:40:50 +0530

    implement index

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const authRouter = require("./router/authRouter");
-const cors = require("cors");
-const socketIo = require("socket.io");
-const http = require("http");
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
-require("./db/mongoose");
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:3000/dashboard",
-    methods: ["GET", "POST"],
-  },
-});
-io.on("connection", (socket) => {
+// const express = require("express");
+// const authRouter = require("./router/authRouter");
+// const cors = require("cors");
+// const socketIo = require("socket.io");
+// const http = require("http");
+// const app = express();
+// app.use(cors());
+// const server = http.createServer(app);
+// require("./db/mongoose");
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "http://localhost:3000/dashboard",
+//     methods: ["GET", "POST"],
+//   },
+// });
+// io.on("connection", (socket) => {
 
-  socket.on("deleteMessage",(message)=>{
-    io.emit("delMessage", message);
-  })
-  socket.on("sendMessage", (message) => {
-    console.log('message', message)
-    io.emit("message", message);
-  });
-});
+//   socket.on("deleteMessage",(message)=>{
+//     io.emit("delMessage", message);
+//   })
+//   socket.on("sendMessage", (message) => {
+//     console.log('message', message)
+//     io.emit("message", message);
+//   });
+// });
 
-const port = process.env.PORT;
-app.use(express.json());
-app.use(authRouter);
+// const port = process.env.PORT;
+// app.use(express.json());
+// app.use(authRouter);
 
-app.get('/',(req,res)=>{
-  res.send("Home")
-})
+// app.get('/',(req,res)=>{
+//   res.send("Home")
+// })
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Server running at port ${port}`);
+// });
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const authRouter = require("./router/authRouter");
+const cors = require("cors");
+const socketIo = require("socket.io");
+const http = require("http");
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+require("./db/mongoose");
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000/dashboard",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+
+  socket.on("deleteMessage",(message)=>{
+    io.emit("delMessage", message);
+  })
+  socket.on("sendMessage", (message) => {
+    console.log('message', message)
+    io.emit("message", message);
+  });
+});
+
+const port = process.env.PORT;
+app.use(express.json());
+app.use(authRouter);
+
+app.get('/',(req,res)=>{
+  res.send("Home")
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
